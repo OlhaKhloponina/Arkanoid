590db4f5de243b75055884a898cc87cb290fd208
Olha Kovalenko <olha.kovalenko@ivkhk.ee>
2025-11-21 18:12:01 +0200

    Обработка столкновения мяча с блоками

--- game.js
+++ game.js
@@ -66,11 +66,19 @@ let game = {
   update(){
     this.platform.move();
     this.ball.move();
-
+    this.collideBlocks();
+    this.collidePlatform();
+  },
+  collideBlocks(){
     for (let block of this.blocks) {
       if (this.ball.collide(block)) {
         this.ball.bumpBlock(block);
       }
+    }
+  },
+  collidePlatform() {
+    if (this.ball.collide(this.platform)) {
+      this.ball.bumpPlatform(this.platform);
     }
   },
   run() {
@@ -139,13 +147,21 @@ game.ball = {
     },
     bumpBlock(block) {
       this.dy *= -1;
+    },
+    bumpPlatform(platform) {
+      this.dy *= -1;
+      let touchX = this.x + this.width / 2;
+      this.dx = this.velocity * platform.getTouchOffset(touchX);
     }
 };
+
 game.platform = {
     velocity: 6,
     dx: 0,
     x: 280,
     y: 300,
+    width: 100,
+    height: 14,
     ball: game.ball,
     fire(){
         if (this.ball) {
@@ -170,6 +186,12 @@ game.platform = {
                 this.ball.x += this.dx;
             }
         }
+    },
+    getTouchOffset(x) {
+      let diff = (this.x + this.width) - x;
+      let offset = this.width - diff;
+      let result = 2 * offset / this.width;
+      return result - 1;
     }
 };
 window.addEventListener("load", () => {
